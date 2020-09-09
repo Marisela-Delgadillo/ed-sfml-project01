c7fb411bb8e6d68266572d15f0591164317922e2
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-09-08 17:47:07 -0700

    project01

---
+++ src/main.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Platform Game");
+    sf::Event event;
+    while(window.isOpen())
+    {
+       
+    }
+    return 0;
+}
